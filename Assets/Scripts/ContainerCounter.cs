622af0194fa1ba06edd15cf3e63a05597387fd7d
Liran Reuven <reulir6@gmail.com>
2023-08-21 22:56:59 +0300

    add animation to conatiner counter

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ContainerCounter : BaseCounter
 {
+    public event EventHandler OnPlayerGrabbedObject;
+
     public override void Interact(Player player)
     {
         Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
         kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
         kitchenObject.SetKitchenObjectParent(player);
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
     }
 
 
